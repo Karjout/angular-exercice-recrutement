12facb5c4bbbfd771f95df3722d84da889464f0e
abdeslam <abdeslam.karjout@uit.ac.ma>
2022-07-12 04:30:49 +0100

    V1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { DataTableModule, SharedModule } from 'primeng/primeng';
-import { StickyHeaderDirective } from './sticky-header.directive';
-import { importType } from '@angular/compiler/src/output/output_ast';
-
 @NgModule({
   declarations: [
     AppComponent,
-    StickyHeaderDirective
+    
   ],
   imports: [
     BrowserModule,
